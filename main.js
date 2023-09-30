ff75d8b6aa0ec8d4030f65e6d413e2b40067d7f5
Unomars4 <une.nyide@gmail.com>
2023-09-30 11:15:40 +0200

    add game skeleton

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 document.querySelector('#app').innerHTML = `
   <div>
-    
+    <h1 class="title">Player against Computer</h1>
+    <p class="instructions">
+        Player goes first: click on a number. First to have a
+        a set of 3 adding to 15 wins. Reload for new game.
+    </p>
+    <section class="aiSection">
+        <p class="aiTitle">Computer</p>
+    </section>
+    <hr/>
+    <section class="boardSection">
+        <p class="boardTitle">Board</p>
+    </section>
+    <hr/>
+    <section class="playerSection">
+        <p class="playerTitle">Player</p>
+    </section>
+    <hr/>
   </div>
 `
 
