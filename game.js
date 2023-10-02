cc8dd20630f4207526ba540ffe783ddfd205cd10
Unomars4 <une.nyide@gmail.com>
2023-10-02 10:17:19 +0200

    add state to game and gameboard modules

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
 //Handles all the game logic,turns, whether its over or not
+import GameBoard from "./gameboard"
+
+const Game = (() => {
+    const players = ["Player", "Computer"]
+    const activePlayer = players[0];
+
+    const switchActivePlayer = () => {
+        return activePlayer == players[0] ? players[1] : players[0];
+    }
+    return {};
+})();
+
+export default Game;
--- gameboard.js
+++ gameboard.js
@@ -1,1 +1,39 @@
 //Handles all the gameboard logic and functionality
+const GameBoard = (() => {
+    //3 numbers that add up to 15
+    const groups = [
+        "",
+        "3 4 8",
+        "1 5 9",
+        "2 6 7",
+        "1 6 8",
+        "3 5 7",
+        "2 4 9",
+        "2 5 8",
+        "4 5 6"
+    ];
+
+    //Tells you which numbers are in which groups(N + 1)
+    const occupied = [
+        [2, 4],
+        [3, 6, 7],
+        [1, 5],
+        [1, 6, 8],
+        [2, 5, 7, 8],
+        [3, 4, 7],
+        [3, 5],
+        [1, 4, 7],
+        [2, 6]
+    ];
+
+    const player = [], computer = [];
+    const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    //influence ai decisions, check game won/lost
+    const pgCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const cpCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    return {};
+})();
+
+export default GameBoard;
